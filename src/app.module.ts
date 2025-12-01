168c70b9e2458cbadc7745c4cdb5b6b84fbae584
mrxx1234m <uzbsamuraypubgm@gmail.com>
2025-12-02 02:43:52 +0500

    udpate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TelegramModule } from './telegram/telegram.module';
 import { TariffModule } from './tariff/tariff.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ PrismaModule,TelegramModule, TariffModule],
+  imports: [
+    PrismaModule,
+    TelegramModule,
+    TariffModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
